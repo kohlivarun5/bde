ddd215feb024a27c70a1dbf27f7ef51e3e4ca1c6
Varun Kohli <kohli_varun5@yahoo.com>
2014-05-09 03:03:05 +0530

    Add decompose as to_string

--- ocaml/groups/bdl/bdldt/src/bdldt_date.c
+++ ocaml/groups/bdl/bdldt/src/bdldt_date.c
@@ -1,23 +1,41 @@
 #include <caml/memory.h>
+#include <caml/alloc.h>
 
 #include <stdio.h>
 
 extern "C" {
 
 CAMLprim value
-bdldt_date_make( value ml_year, value ml_month, value ml_day)
+bdldt_date_make(value ml_year, value ml_month, value ml_day)
 {
-    CAMLparam3( ml_year, ml_month, ml_day);
-
     const int year  = Int_val(ml_year);
     const int month = Int_val(ml_month);
     const int day   = Int_val(ml_day);
 
-    const int date_int = year + month + day;
-
-    printf("Date is :{%d}   <<----->> %d:%s\n",date_int,__LINE__,__FILE__);
+    const int date_int = (year*10000) + (month*100) + day;
 
     return Val_int(date_int);
 }
+
+CAMLprim value
+bdldt_date_decompose(value ml_date_int)
+{
+    const int date_int = Int_val(ml_date_int);
+
+    CAMLparam1( ml_date_int );
+    CAMLlocal1( ml_tuple );
+    ml_tuple = caml_alloc(3, 0);
+
+    const int year = date_int / 10000;
+    const int month = (date_int % 10000) / 100;
+    const int day = (date_int % 100);
+
+    Store_field( ml_tuple, 0, year);
+    Store_field( ml_tuple, 1, month);
+    Store_field( ml_tuple, 2, day);
+
+    CAMLreturn( ml_tuple );
+}
+
 
 }
--- ocaml/groups/bdl/bdldt/test/test_bdldt_date.ml
+++ ocaml/groups/bdl/bdldt/test/test_bdldt_date.ml
@@ -1,4 +1,12 @@
 
 let () = 
-  let _ = Bdldt_date.make 1 1 1 in
+  let date = Bdldt_date.make ~year:2014 ~month:12 ~day:20 in
+  let (year,month,day) = Bdldt_date.decompose date in
+  let s1 = Printf.sprintf "%d-%d-%d" year month day in
+  let _  = Printf.printf "s1 : {%s}\n" s1 in
+  let s2 = Bdldt_date.to_string date in
+  let _  = Printf.printf "s2 : {%s}\n" s2 in
+  let _ = Printf.printf 
+            "The strings are %s\n" 
+            (if s1 = s2 then "same" else "different") in
   ()
