5df0ddc133ec9570081ae4024ae7810c9e5c82eb
Varun Kohli <kohli_varun5@yahoo.com>
2014-06-08 21:28:52 -0400

    Add bsl

--- ocaml/setup.sh
+++ ocaml/setup.sh
@@ -5,7 +5,7 @@ BDE_BUILD_DIR=$PWD/../build/Debug/
 
 BSL_HEADERS=`\ls -d1 $BDE_SOURCE_ROOT/groups/bsl/*/`
 #echo $BSL_HEADERS 
-BSL_HEADERS_WITH_I=(${BSL_HEADERS[@]//\/Users/-I/Users})
+BSL_HEADERS_WITH_I=(${BSL_HEADERS[@]/#/-I})
 
 
 export BSL_HEADERS_WITH_I=$( IFS=$' '; echo "${BSL_HEADERS_WITH_I[*]}" )
