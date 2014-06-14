b1587625ce3cc7f600ebb64c710fbcb514a69c42
Varun Kohli <kohli_varun5@yahoo.com>
2014-06-14 14:13:14 -0400

    Fix headers

--- ocaml/setup.sh
+++ ocaml/setup.sh
@@ -5,12 +5,13 @@ BDE_BUILD_DIR=$PWD/../build/Debug/
 
 BSL_HEADERS=`\ls -d1 $BDE_SOURCE_ROOT/groups/bsl/*/`
 #echo $BSL_HEADERS 
-BSL_HEADERS_WITH_I=(${BSL_HEADERS[@]/#/-I})
-
-
-export BSL_HEADERS_WITH_I=$( IFS=$' '; echo "${BSL_HEADERS_WITH_I[*]}" )
-export BSL_HEADERS_WITH_I="${BSL_HEADERS_WITH_I}"
-
+export BSL_HEADERS_WITH_I=
+BSL_HEADERS_WITH_I= 
+for i in $BSL_HEADERS
+do 
+    BSL_HEADERS_WITH_I="$BSL_HEADERS_WITH_I -I$i"
+done
+export BSL_HEADERS_WITH_I
 export BSL_LIB=$BDE_BUILD_DIR/groups/bsl/libbsl.a
 
 echo "BSL Headers = ${BSL_HEADERS_WITH_I}"
