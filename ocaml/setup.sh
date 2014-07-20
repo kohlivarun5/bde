cc5108b6e7ada384faa8a2f0a6ee6c407b25debc
Varun Kohli <kohli_varun5@yahoo.com>
2014-07-20 13:47:10 -0400

    fix

--- ocaml/setup.sh
+++ ocaml/setup.sh
@@ -14,6 +14,15 @@ done
 export BSL_HEADERS_WITH_I
 export BSL_LIB=$BDE_BUILD_DIR/groups/bsl/libbsl.a
 
+
+export WARN_ERROR_FLAGS='-warn-error -w -verbose'
+export OCAMLC_CMD='ocamlc $(WARN_ERROR_FLAGS) -c'
+export OCAMLOPT_CMD='ocamlopt $(WARN_ERROR_FLAGS) -c'
+export OCAMLLIB=`ocamlc -where`
+export OCAML_INCLUDE=$OCAMLLIB
+
+export CPP_LIBSTD_FLAGS='-cclib -L$(PWD)/ -cclib -lstdc++'
+
 echo "BSL Headers = ${BSL_HEADERS_WITH_I}"
 echo
 echo "BSL LIB     = ${BSL_LIB}"
